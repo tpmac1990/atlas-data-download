07100a9375e07aa8d90ab1332e9893b75a5454a8
tpmac1990 <terence.paulmac@gmail.com>
2022-04-29 19:10:57 +1000

    required_task improved

--- functions/segment/required_task.py
+++ functions/segment/required_task.py
@@ -7,40 +7,59 @@ import datetime
 
 from functions.common.directory_files import get_json, write_json, file_exist
 from functions.common.database_commands import drop_and_restore_db
-from ..setup import SetUp, Logger, update_setup
+from ..setup import SetUp, update_setup
+from functions.logging.logger import logger
 from functions.common import pre_segment
 from functions.common.backup import complete_script__backup
 
 
-
 class PromptRequiredTask:
-    """
-    1. Prompts the user on what they want to run and then sets the configs in run_tracker.json
-    2. Fires off the segments that requested to be run 
+    """ 
+    Here, the run configs are set. The user can choose whether to run the 'complete_script' or run a single or
+    multiple 'segments'.
+    complete_script: 
+        1. download state source data
+        2. update core data with user edits from the remote db
+        3. load manual updates in to the db
+    segments: run indivdual modules of code that make up the complete_script. This enables a single segment to 
+    be run in unison. Each segment has a dataset and db_dump (if required) that is set before the segment is run.
+    There are three different directories, each also has its own database:
+        1. remote: used for production. Segments can not be used on this, as doing so would overwrite the production data.
+        2. local: used to run as if remote without affecting the production data. allows complete_script and segments
+        3. testing: used to test new features. allows complete_script and segments
+    local and testing are the same in nature, but having two allows the user to test something without affecting the local
+    dataset and having to re-run it.
     """
     run_tracker_path = os.path.join(SetUp.configs_dir, 'run_tracker.json')
     run_tracker_config = get_json(run_tracker_path)
 
-
     def all_steps(self):
         """ Go through all the required prompts to figure out what steps to take """
-        Logger.logger.info(f"\n\n{Logger.hashed}\nSetting run configurations\n{Logger.hashed}")
-        
+        logger(message="Setting run configurations", category=1)
         if self.use_last_configs():
+            self.trigger_run_module()
+            
+        else:
+            self.set_directory_to_use()
+            self.set_modules_to_run()
+            self.set_task_to_run()
+            # this will be skipped if 'complete_script' has been selected
+            self.set_segments_to_run()
+            self.trigger_run_module()
+            
+            
+    def trigger_run_module(self):
+        """ 
+        If the run_module equals 'full_script' then a back up will be made and the script will
+        naturally exit this module and continue on with the full script. If 'segments' then
+        the segments from the run_configs.json file will be run. On completion, the function will be exited
+        before it has the chance to continue on running the complete script.
+        """
+        if self.run_complete_script():
             complete_script__backup()
-            return
-
-        self.set_directory_to_use()
-        
-        self.set_modules_to_run()
-        
-        self.set_task_to_run()
-        
-        self.set_segments_to_run()
-        
-        complete_script__backup()
-        
-    
+        else:
+            self.run_segments()
+            os._exit(1)
     
     def use_last_configs(self):
         """
@@ -71,19 +90,13 @@ class PromptRequiredTask:
 
     
     def set_directory_to_use(self):
-        """ 
-        Set the directory for the dataset to use. If running pytest then the 'testing'
-        directory will be automatically selected
-        """
-        if 'pytest' in sys.modules:
-            directory = 'testing'
-        else:
-            msg = "Which data directory would you like to use?\n(1) - Production\n(2) - Local\n(3) - Test\n"
-            choice = input(msg)
-            dic = {"1": "production", "2": "local", "3": "testing"}
-            directory = dic[choice]
+        """ Set the directory for the dataset to use """
+        msg = "Which data directory would you like to use?\n(1) - Production\n(2) - Local\n(3) - Test\n"
+        choice = input(msg)
+        dic = {"1": "production", "2": "local", "3": "testing"}
+        directory = dic[choice]
 
-        Logger.logger.info(f"User has selected to use directory: '{directory}'")
+        logger(message=f"User has selected to use directory: '{directory}'", category=4)
         # save the directory to run_tracker.json so SetUp object can read it
         self.run_tracker_config["active_atlas_directory_name"] = directory
         
@@ -105,18 +118,17 @@ class PromptRequiredTask:
             dic = {"1": "complete_script", "2": "segment"}
             run_module = dic[choice]
         
-        Logger.logger.info(f"User has selected to run: '{run_module}'")
+        logger(message=f"User has selected to run: '{run_module}'", category=4)
+        
         self.run_tracker_config[directory]["run_module"] = run_module
         if run_module == "segment":
             self.run_tracker_config[directory]["task"] = ""
             
         # create back up of files and database before running complete script. They will be re-instated on error
         if run_module == "complete_script":
-            """
-             1. copy folders from one place to another if they exist
-             2. create database dump and save it to db_dumps
-             3. backup folder: SetUp.backup_dir
-            """
+            #  1. copy folders from one place to another if they exist
+            #  2. create database dump and save it to db_dumps
+            #  3. backup folder: SetUp.backup_dir
             complete_script__backup()
 
             
@@ -129,14 +141,14 @@ class PromptRequiredTask:
             ## only run when complete_script is requested ##
         '''
         directory = self.run_tracker_config["active_atlas_directory_name"]
-        run_module = self.run_tracker_config[directory]["run_module"]
         
-        # if complete_script then see if manual_update_required needs to be run
-        if not run_module == "complete_script":
+        # if complete_script then see if manual_update_required needs to be rund
+        if not self.run_complete_script():
             return
         
         manual_update_path = os.path.join(SetUp.output_dir, 'update', 'manual_update_required.csv')
         manual_update_df = pd.read_csv(manual_update_path)
+
         # options are: state sources or updating user updates
         last_task = self.run_tracker_config[directory]["task"]
         
@@ -144,8 +156,8 @@ class PromptRequiredTask:
         total_count = len(df.index)
         update_rows_count = len(df[~df['LIKELY_MATCH'].isnull()])
 
-        Logger.logger.info(f"The 'manual_update_required' table contains a total of '{total_count}' rows with a provided update value for '{update_rows_count}' of these rows.")
-        Logger.logger.info(f"Last task run: '{last_task}'")
+        logger(message=f"The 'manual_update_required' table contains a total of '{total_count}' rows with a provided update value for '{update_rows_count}' of these rows.", category=4)
+        logger(message=f"Last task run: '{last_task}'", category=4)
 
         msg = (
             f"The 'manual_update_required' table contains a total of '{total_count}' rows with a"
@@ -161,25 +173,25 @@ class PromptRequiredTask:
 
         if choice == '1':
             if update_rows_count == 0:
-                Logger.logger.info(f"User has selected to run the 'manual_update' task, however there is no data in the 'manual_update_required' file to apply. This process will be terminated.")
+                logger(message=f"User has selected to run the 'manual_update' task, however there is no data in the 'manual_update_required' file to apply. This process will be terminated.", category=4)
                 print("You have selected to run the 'manual_update' task, however there is no data in the 'manual_update_required' table to apply.\nExiting process.")
-                sys.exit(1)
+                raise KeyError(f"{choice} was not a viable option")
             new_task = 'manual_updates'
         elif choice == '2':
             new_task = 'state_source_updates'
         else:
-            Logger.logger.info(f"The user exited the function")
+            logger(message=f"The user exited the function", category=4)
             print('No task was selected, Exiting process')
-            sys.exit(1)
-            
-
-        Logger.logger.info(f"User has selected to run task: '{new_task}'")
+            raise KeyError(f"{choice} was not an option")
+        
+        logger(message=f"User has selected to run task: '{new_task}'", category=4)
         print(f"Preparing to run task: '{new_task}'")
 
         self.run_tracker_config[directory]['task'] = new_task
         
         # save configs
         self.save_run_tracker_configs()
+
         
         
     def set_segments_to_run(self):
@@ -192,6 +204,7 @@ class PromptRequiredTask:
         
         # replace the necessary folders so the correct data is in place before the test is run
         test_configs = get_json(os.path.join(SetUp.configs_dir,'end_to_end_tests.json'))
+        
         test_data_dir = SetUp.test_data_dir
         directories = SetUp.directories
 
@@ -216,8 +229,22 @@ class PromptRequiredTask:
         # store the test choices
         self.run_tracker_config[directory]['segments']['group'] = group_choice
         self.run_tracker_config[directory]['segments']['subgroups'] = segment_choices
-            
-        # check for any pre-run_prompts before starting
+        
+        # save configs
+        self.save_run_tracker_configs()
+        
+    
+    def run_segments(self):
+        
+        directory = self.run_tracker_config["active_atlas_directory_name"]
+        group_choice = self.run_tracker_config[directory]['segments']['group']
+        segment_choices = self.run_tracker_config[directory]['segments']['subgroups']
+        test_configs = get_json(os.path.join(SetUp.configs_dir,'end_to_end_tests.json'))
+        segments_configs = test_configs[group_choice]['tests']
+        test_data_dir = SetUp.test_data_dir
+        directories = SetUp.directories
+        
+        # check for any pre-run_prompts before starting e.g. new download data may need be manually downloaded before hand
         for segment_name in segment_choices:
             print(f"Starting: {segment_name}")
             if not 'pre-run_prompt' in segments_configs[segment_name].keys():
@@ -227,10 +254,7 @@ class PromptRequiredTask:
             answer = input(msg)
             self.exit_if_no(answer)
         
-        # save configs
-        self.save_run_tracker_configs()
-        
-        Logger.logger.info("User has selected test group '%s' & sub-group '%s'"%(group_choice,(', ').join(segment_choices)))
+        logger(message="User has selected test group '%s' & sub-group '%s'"%(group_choice,(', ').join(segment_choices)), category=4)
         # loop through each segment and run its associated module/method
         for segment_name in segment_choices:
             selected_test = segments_configs[segment_name]
@@ -311,7 +335,7 @@ class PromptRequiredTask:
                 try:
                     spread_1 = int(dash_split[0])
                     spread_2 = int(dash_split[1])
-                    if spread_2 >= spread_1:
+                    if spread_1 >= spread_2:
                         raise ValueError
                 except ValueError:
                     raise ValueError(error_msg)
@@ -345,6 +369,7 @@ class PromptRequiredTask:
             
     @staticmethod   
     def yes_no_boolean(answer):
+        """ converts 'yes' to True and anyother answer to False """
         if answer.lower() in ["y","yes"]:
             return True 
         return False
@@ -353,4 +378,13 @@ class PromptRequiredTask:
         ''' save the run configs to the run_tracker.json file and reload SetUp with the new configs '''
         write_json(self.run_tracker_path,self.run_tracker_config)
         update_setup()
+        
+    def run_complete_script(self):
+        """ return true if user has chosen to run the full script, false means the user has chosen to run a segment """
+        directory = self.run_tracker_config["active_atlas_directory_name"]
+        run_module = self.run_tracker_config[directory]["run_module"]
+        # No need to run if not segment as the complete_script will have already been run.
+        if run_module == "complete_script":
+            return True
+        return False
         
